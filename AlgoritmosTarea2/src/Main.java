e63fd0c7d42e11a3fb51cffd7497ad43965b6c8a
mcisterna <matias.cisterna.m@outlook.com>
2014-11-05 22:26:25 -0300

    imagenes informe

--- AlgoritmosTarea2/src/Main.java
+++ AlgoritmosTarea2/src/Main.java
@@ -62,7 +62,7 @@ public class Main {
 		double search_seq2[][][] = new double[2][3][5];
 		double search_seq3[][][] = new double[2][3][5];
 		
-		for(int n=1024;n <= 1024*1024; n*=2){
+		for(int n=1024;n <= 1024*2; n*=2){
 			potencia = (int)(Math.log(n)/Math.log(2));
 			Kn = new int[n];
 			seq1 = new int[100*n];
